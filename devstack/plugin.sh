b909a17a81cfbc4811868e3ad4fe9a332d6544ce
MariaCascone66 <155472232+MariaCascone66@users.noreply.github.com>
2025-02-16 19:33:15 +0100

    Update plugin.sh to try if it works with a basic echos

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
-# plugin.sh - DevStack plugin.sh dispatch script template
 
-function install_template {
-    echo "INSTALL"
-}
-
-function init_template {
-    echo "INIT"
-}
-
-function configure_template {
-    echo "CONFIGURE"
-}
-
-# check for service enabled
-if is_service_enabled template; then
-
-    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
-        # Set up system services
-        echo_summary "Configuring system services Template"
-        install_package cowsay
-
-    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
-        # Perform installation of service source
-        echo_summary "Installing Template"
-        install_template
-
-    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-        # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring Template"
-        configure_template
-
-    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
-        # Initialize and start the template service
-        echo_summary "Initializing Template"
-        init_template
-    fi
-
-    if [[ "$1" == "unstack" ]]; then
-        # Shut down template services
-        # no-op
-        :
-    fi
-
-    if [[ "$1" == "clean" ]]; then
-        # Remove state and transient data
-        # Remember clean.sh first calls unstack.sh
-        # no-op
-        :
-    fi
-fi
+for i in {1..50}; do
+    echo "THIS IS A MESSAGE IN CAPS"
+done
