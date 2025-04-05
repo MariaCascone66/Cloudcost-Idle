50a4695fbe0eff069921c517441547fb45d17a8e
MariaCascone66 <mariacascone596@gmail.com>
2025-04-05 18:15:58 +0000

    changing directory and path

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,27 +3,29 @@
 function install_snapshot-scheduler {
     echo "Installing snapshot-scheduler plugin"
     
-    # Crea il virtualenv (se non esiste)
+    # Crea il virtualenv 
     if [ ! -d "/opt/stack/snapshot-scheduler/venv" ]; then
-        python3 -m venv /opt/stack/snapshot-scheduler/venv
+        python3 -m venv "/opt/stack/snapshot-scheduler/venv"
     fi
 
     # Attiva l'ambiente e installa i pacchetti
-    source /opt/stack/snapshot-scheduler/venv/bin/activate
-    pip install --upgrade pip
+    source "/opt/stack/snapshot-scheduler/venv/bin/activate"
     
-    # Se esiste requirements.txt, installa i pacchetti da lì
+    # Install Requirements
     if [ -f "/opt/stack/snapshot-scheduler/app/requirements.txt" ]; then
-        pip install -r /opt/stack/snapshot-scheduler/app/requirements.txt
+        pip install -r "/opt/stack/snapshot-scheduler/app/requirements.txt" || { echo "Failed to install dependencies"; exit 1; }
+    else
+        echo "requirements.txt not found!"
+        exit 1
     fi
 
      # Installa manualmente scheduler.py
-    pip install /opt/stack/snapshot-scheduler/app/scheduler.py
+    pip install -r "/opt/stack/snapshot-scheduler/app/scheduler.py"
 }
 
 function start_snapshot-scheduler {
     echo "Starting snapshot-scheduler service"
-    sudo cp /opt/stack/snapshot-scheduler/snap-scheduler.service /etc/systemd/system/
+    sudo cp "/opt/stack/snapshot-scheduler/snap-scheduler.service" "/etc/systemd/system/"
     sudo systemctl daemon-reexec
     sudo systemctl daemon-reload
     sudo systemctl enable snap-scheduler.service
