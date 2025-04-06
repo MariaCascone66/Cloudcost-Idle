206737d8f2b61384edf1c0ebc823e9ded8efc057
MariaCascone66 <mariacascone596@gmail.com>
2025-04-06 16:52:45 +0000

    fix directory, changing in python3

--- scheduler/start-scheduler.sh
+++ scheduler/start-scheduler.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Carica le credenziali OpenStack
-source /opt/stack/devstack/openrc admin admin
+source "/opt/stack/devstack/openrc admin admin"
 
 # Attiva il virtualenv
-source /opt/stack/snapshot-scheduler/venv/bin/activate
+source "/opt/stack/snapshot-scheduler/app/venv/bin/activate"
 
 # Avvia lo scheduler
-python /opt/stack/snapshot-scheduler/app/scheduler.py
+python3 "/opt/stack/snapshot-scheduler/app/scheduler.py"
 
-chmod +x /opt/stack/snapshot-scheduler/scheduler/start-scheduler.sh
+chmod +x "/opt/stack/snapshot-scheduler/scheduler/start-scheduler.sh"
