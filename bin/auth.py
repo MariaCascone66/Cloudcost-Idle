a11a8910916b108f2b950737fb0c4b46c8df4e54
MariaCascone66 <mariacascone596@gmail.com>
2025-04-17 17:33:00 +0000

    cambio in auth_args

--- bin/auth.py
+++ bin/auth.py
@@ -18,20 +18,8 @@ def get_openstack_connection():
             'region_name': os.environ['OS_REGION_NAME'],
             'volume_api_version': os.environ['OS_VOLUME_API_VERSION'],
         }
-
-        # Authentication configuration
-        auth = v3.Password(
-            auth_url=auth_url,
-            auth_type=auth_type,
-            username=username,
-            password=password,
-            project_name=project_name,
-            user_domain_id=user_domain_id,
-            project_domain_id=project_domain_id,
-            region_name=region_name,
-            volume_api_version=volume_api_version,
-        )
-
+        
+        auth = v3.Password(**auth_args)
         # Create a session
         sess = session.Session(auth=auth)
         return connection.Connection(session=sess, region_name=os.environ.get('OS_REGION_NAME', 'RegionOne'))
