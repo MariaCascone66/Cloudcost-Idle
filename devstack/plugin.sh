e7d6478225327890f5bcab4362d17fbf8a8c9f6b
MariaCascone66 <mariacascone596@gmail.com>
2025-07-21 10:56:05 +0000

    modifica struttura plugin.sh + aggiunta di PyMySQL

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,7 +2,7 @@
 # === Load plugin settings ===
 source ${BASH_SOURCE%/*}/settings
 
-function install_flask_dependencies() {
+function install_flask_dependencies {
     echo "Installing Flask and dependencies..."
     if [[ ! -d "$APP_DIR/venv" ]]; then
         python3 -m venv "$APP_DIR/venv"
@@ -12,48 +12,48 @@ function install_flask_dependencies() {
     deactivate
 }
 
-function copy_service_file() {
+function install_nova_filter {
+    echo "Installing IdleVMFilter into Nova..."
+    sudo cp "$APP_DIR/nova_filter/idle_vm_filter.py" /opt/stack/nova/nova/scheduler/filters/
+}
+
+function configure_nova_filter {
+    echo "Enabling IdleVMFilter in nova.conf..."
+    iniset /etc/nova/nova.conf DEFAULT scheduler_available_filters nova.scheduler.filters.all_filters
+    iniset /etc/nova/nova.conf DEFAULT scheduler_default_filters RetryFilter,AvailabilityZoneFilter,IdleVMFilter
+}
+
+function restart_nova_scheduler {
+    echo "Restarting Nova Scheduler..."
+    sudo systemctl restart devstack@n-sch.service
+}
+
+function copy_service_file {
     echo "Copying service file to systemd directory..."
     sudo cp "$SERVICE_DIR/cloudcost_idle.service" "$SYSTEMD_DIR"
     sudo systemctl daemon-reload
     sudo systemctl enable cloudcost_idle.service
 }
 
-function start_plugin() {
+function start_plugin {
     echo "Starting plugin..."
     sudo systemctl start cloudcost_idle.service
 }
 
-function stop_plugin() {
+function stop_plugin {
     echo "Stopping plugin..."
     sudo systemctl stop cloudcost_idle.service
-}
-
-function install_nova_filter() {
-    echo "Installing IdleVMFilter into Nova filters..."
-    sudo cp "$APP_DIR/nova_filter/idle_vm_filter.py" /opt/stack/nova/nova/scheduler/filters/
-}
-
-function configure_nova_filter() {
-    echo "Enabling IdleVMFilter in nova.conf..."
-    iniset /etc/nova/nova.conf DEFAULT scheduler_available_filters nova.scheduler.filters.all_filters
-    iniset /etc/nova/nova.conf DEFAULT scheduler_default_filters RetryFilter,AvailabilityZoneFilter,IdleVMFilter
-}
-
-function restart_nova_scheduler() {
-    echo "Restarting nova scheduler service..."
-    sudo systemctl restart devstack@n-sch.service || sudo systemctl restart nova-scheduler
 }
 
 if is_service_enabled cloudcost_idle; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
         install_flask_dependencies
-        copy_service_file
         install_nova_filter
         configure_nova_filter
+        copy_service_file
+        restart_nova_scheduler
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         start_plugin
-        restart_nova_scheduler
     elif [[ "$1" == "unstack" ]]; then
         stop_plugin
     fi
